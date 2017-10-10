e5ce72940a412c85caee28a60464fbda4e8963e1
amit.k@syncoms.com <siddharth.ubale@Siddharth.India.CIDOMAIN.local>
2017-10-10 18:02:55 +0530

    update1

---
+++ src/main/java/symphoni/test124.java
@@ -0,0 +1,14 @@
+package symphoni;
+
+public class test124 {
+
+	public static void main(String[] args)
+	{
+		
+		String name="abd";
+		int age=90;
+		
+		System.out.println("Name : "+name+" & age: "+age+"");
+		
+	}
+}
