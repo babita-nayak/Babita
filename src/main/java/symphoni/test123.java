f576848a4bf543d7c0427598d41540f83acdcbbf
amit.k@syncoms.com <siddharth.ubale@Siddharth.India.CIDOMAIN.local>
2017-10-10 16:07:45 +0530

    test 1

--- src/main/java/symphoni/test123.java
+++ src/main/java/symphoni/test123.java
@@ -5,7 +5,7 @@ public class test123 {
 	public static void main(String args[])
 	{
 		String name= "Symphoni";
-		System.out.println("Helloo!" + name);
+		System.out.println("Helloo !" + name);
 		
 	}
 
